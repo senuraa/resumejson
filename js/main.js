a626175300a38c980e55df49bd840237d0851798
Senura Seneviratne <senuraa@msn.com>
2017-02-22 13:09:41 +0000

    Resume finalized

--- js/main.js
+++ js/main.js
@@ -15,6 +15,9 @@ $(document).ready(function() {
     headerdata();
     edudata();
     workdata();
+    skillsdata();
+    extradata();
+    awardsdata();
 });
 
 function headerdata() {
@@ -42,4 +45,36 @@ function workdata(){
         txthtml+= "<div class='row'><div class='three columns left--pane'><h6>"+json.work[i].company+"</h6>"+"<p class='years'>"+json.work[i].startyear+"-"+json.work[i].endyear+"</p></div>"+"<div class='nine columns right--pane'><p class='title--right'>"+json.work[i].position+"</p><p class='desc'>"+json.work[i].summary+"</p></div></div>"
     }
     document.getElementById("work").innerHTML=txthtml;
+}
+function skillsdata(){
+     var txthtml, arrlength, i,j,arrlength2;
+    arrlength = json.skills.length;
+    txthtml = "";
+    for (i=0;i<arrlength;i++){
+        txthtml+="<div class='row'><div class='three columns left--pane'><h6>"+json.skills[i].title+"</h6></div><div class='nine columns right--pane'>";
+        arrlength2 = json.skills[i].list.length;
+        for(j=0;j<arrlength2;j++){
+            txthtml+="<p class='title--right'>"+json.skills[i].list[j].section+"</p><p class='desc'>"+json.skills[i].list[j].list+"</p>"
+        }
+        txthtml+="</div></div>";
+    }
+    document.getElementById("skills").innerHTML=txthtml;
+}
+function extradata(){
+    var txthtml, arrlength, i;
+    arrlength = json.extra.length;
+    txthtml = "";
+    for (i=0;i<arrlength;i++){
+        txthtml+= "<div class='row'><div class='three columns left--pane'><h6>"+json.extra[i].institution+"</h6>"+"<p class='years'>"+json.extra[i].year+"</p></div>"+"<div class='nine columns right--pane'><p class='title--right'>"+json.extra[i].position+"</p><p class='desc'>"+json.extra[i].summary+"</p></div></div>"
+    }
+    document.getElementById("extra").innerHTML=txthtml;
+}
+function awardsdata(){
+    var txthtml, arrlength, i;
+    arrlength = json.awards.length;
+    txthtml = "";
+    for (i=0;i<arrlength;i++){
+        txthtml+= "<div class='row'><div class='three columns left--pane'><h6>"+json.awards[i].name+"</h6>"+"<p class='years'>"+json.awards[i].year+"</p></div>"+"<div class='nine columns right--pane'><p class='title--right'>"+json.awards[i].for+"</p><p class='desc'>"+json.awards[i].summary+"</p></div></div>"
+    }
+    document.getElementById("awards").innerHTML=txthtml;
 }
